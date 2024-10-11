ad0091f24046dd8eea319f3543a5e586525387aa
Klastiv <gyarfas.reka@gmail.com>
2024-10-11 13:22:46 +0200

    Comment left on change

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -42,7 +42,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount = -numberOfTorpedos;
+      this.torpedoCount = -numberOfTorpedos; //a space added for better readability of = -
       success = true;
     } else {
       // simulated failure
